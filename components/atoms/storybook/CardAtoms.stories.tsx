8d0828896ba7e908b5e106efbc569cc6cd645f91
roll1226 <tomo_yasu_4724@icloud.com>
2021-02-01 22:04:25 +0900

    feat: テキストstyle

--- components/atoms/storybook/CardAtoms.stories.tsx
+++ components/atoms/storybook/CardAtoms.stories.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { withKnobs, number, boolean } from "@storybook/addon-knobs";
 import CardAtoms from "../CardAtoms";
+import {
+  GeneralText,
+  GeneralFontSize,
+  GeneralFontWeight,
+  GeneralTextParagraph,
+} from "../../../styles/typography/GeneralTextStyle";
 
 export default {
   title: "Components/Atoms/Card",
@@ -10,6 +16,15 @@ export default {
 
 export const showCardAtoms = () => (
   <CardAtoms width={number("Width", 400)} raised={boolean("Raised", false)}>
-    <p>ここにchildrenを入れる</p>
+    <GeneralText
+      fontSize={GeneralFontSize.SIZE_24}
+      fontWeight={GeneralFontWeight.BOLD}
+    >
+      タイトル
+    </GeneralText>
+
+    <GeneralTextParagraph fontSize={GeneralFontSize.SIZE_16}>
+      テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+    </GeneralTextParagraph>
   </CardAtoms>
 );
