3fa6258c297a07145a7df89aa5913bb841e72fac
kurauchi0611 <5kuu0611@gmail.com>
2021-03-03 00:02:53 +0900

    add googlemap

--- pages/movieInfo/[id]/reservation.tsx
+++ pages/movieInfo/[id]/reservation.tsx
@@ -1,9 +1,12 @@
 import CardAtoms from "../../../components/atoms/CardAtoms";
-import GeneralText from "../../../styles/typography/GeneralTextStyle";
+import GeneralText, {
+  GeneralFontWeight,
+} from "../../../styles/typography/GeneralTextStyle";
 import { GeneralFontSize } from "../../../styles/typography/GeneralTextStyle";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { db, auth } from "../../../utils/firebase/firebase";
+import ja from "date-fns/locale/ja";
 import { format } from "date-fns";
 import {
   GeneralFlex,
@@ -47,7 +50,13 @@ const ReservationMovie = () => {
       .add({
         ...movie,
       });
-    await db.collection("schedule").doc(id).set({wantWatch:firebase.firestore.FieldValue.increment(1)},{merge:true})
+    await db
+      .collection("schedule")
+      .doc(id)
+      .set(
+        { wantWatch: firebase.firestore.FieldValue.increment(1) },
+        { merge: true }
+      );
     router.push("/movieInfo/complete");
   };
 
@@ -64,24 +73,48 @@ const ReservationMovie = () => {
             <GeneralSpacer horizontal={16} />
             <CardAtoms width={376} raised={true}>
               <GeneralText fontSize={GeneralFontSize.SIZE_12}>作品</GeneralText>
-              <GeneralText fontSize={GeneralFontSize.SIZE_12}>
+              <GeneralText
+                fontSize={GeneralFontSize.SIZE_12}
+                fontWeight={GeneralFontWeight.BOLD}
+              >
                 {movie.movie.title}
               </GeneralText>
-
+              <GeneralSpacer horizontal={16} />
               <GeneralText fontSize={GeneralFontSize.SIZE_12}>日時</GeneralText>
-              <GeneralText fontSize={GeneralFontSize.SIZE_12}>
-                2020年12月19日（土）
+              <GeneralText
+                fontSize={GeneralFontSize.SIZE_12}
+                fontWeight={GeneralFontWeight.BOLD}
+              >
+                {`${format(movie.openTime.toDate(), "yyyy年MM月dd日（E）", {
+                  locale: ja,
+                })}`}
               </GeneralText>
-              <GeneralText fontSize={GeneralFontSize.SIZE_12}>
-                開場：17:20〜　開演：18:20〜
+              <GeneralText
+                fontSize={GeneralFontSize.SIZE_12}
+                fontWeight={GeneralFontWeight.BOLD}
+              >
+                {`開場：${format(movie.openTime.toDate(), "HH:mm ~", {
+                  locale: ja,
+                })}　開演：${format(movie.startTime.toDate(), "HH:mm ~", {
+                  locale: ja,
+                })}`}
               </GeneralText>
-
+              <GeneralSpacer horizontal={16} />
               <GeneralText fontSize={GeneralFontSize.SIZE_12}>会場</GeneralText>
-              <GeneralText fontSize={GeneralFontSize.SIZE_12}>
-                青森市民ホール
+              <GeneralText
+                fontSize={GeneralFontSize.SIZE_12}
+                fontWeight={GeneralFontWeight.BOLD}
+              >
+                {movie.place.prefecture}
+                {movie.place.city}
                 <br />
-                青森県青森市柳川１丁目２−１４
+                {movie.place.other}
               </GeneralText>
+              <iframe
+                src={`https://www.google.com/maps?output=embed&q=${movie.place.prefecture} ${movie.place.city} ${movie.place.other}`}
+                style={{ border: 0, width: "100%" }}
+                loading="lazy"
+              ></iframe>
             </CardAtoms>
             <GeneralSpacer horizontal={16} />
           </GeneralFlex>
